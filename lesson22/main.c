e29cbe68fc2a4955997256d0afd067d498170197
fbmnds <fbmnds7@fn.de>
2019-04-15 15:15:39 +0200

    Lesson 22 - task switch through SP manipulation

---
+++ lesson22/main.c
@@ -0,0 +1,39 @@
+#include <stdint.h>
+#include "bsp.h"
+
+
+
+void main_blinky1() {
+    while (1) {
+        BSP_ledGreenOn();
+        BSP_delay(BSP_TICKS_PER_SEC / 4U);
+        BSP_ledGreenOff();
+        BSP_delay(BSP_TICKS_PER_SEC * 3U / 4U);
+    }
+}
+
+void main_blinky2() {
+    while (1) {
+        BSP_ledBlueOn();
+        BSP_delay(BSP_TICKS_PER_SEC / 2U);
+        BSP_ledBlueOff();
+        BSP_delay(BSP_TICKS_PER_SEC / 3U);
+    }
+}
+
+
+/* background code: sequential with blocking version */
+int main() {
+    uint32_t volatile run = 0U;
+
+	BSP_init();
+
+	if (run) {
+	    main_blinky1();
+    }
+	else{
+        main_blinky2();
+    }
+
+    //return 0;
+}
